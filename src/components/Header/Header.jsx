b4d6394c59123c3cc1f7163267dacdbc52f7ab8d
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2023-08-16 16:42:03 +0530

    full updated

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import menu from "../../img/menu-svgrepo-com.svg";
+import search from "../../img/search-svgrepo-com.svg";
+import cart from "../../img/cart-shopping-svgrepo-com.svg";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 z-[100] w-full p-5 bg-my-background flex items-center justify-between drop-shadow-header-shadow">
+      {/* header left */}
+      <div className="flex items-center">
+        <img
+          src={menu}
+          alt="menu"
+          className="w-6 h-6 object-contain cursor-pointer"
+        />
+        <h1 className=" ml-2 text-sm font-bold">
+          RAVI <span className="text-yellow-900">DEV</span>
+        </h1>
+        <div className=" overflow-hidden ml-2 flex items-center rounded-full bg-[#e7eee7]">
+          <input
+            type="text"
+            placeholder="Search"
+            className="ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit"
+          />
+          <img
+            src={search}
+            alt="Search"
+            className="mr-2 w-6 h-6 object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+      {/* Header Right */}
+      <img
+        src={cart}
+        alt="cart"
+        className="w-6 h-6 object-contain cursor-pointer"
+      />
+    </header>
+  );
+};
+
+export default Header;
